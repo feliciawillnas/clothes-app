e5e6105ba5da4efea307831bab3e6aea03d8dbb1
felicia <feliciawillnas@gmail.com>
2023-03-26 03:21:52 +0200

    basic styling

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,3 +1,40 @@
-function Filter() {
-  return <div>hej</div>;
+import styled from "styled-components";
+
+export function Filter() {
+  return (
+    <StyledWrapper>
+      <StyledLogo></StyledLogo>
+      <StyledAllLinks>
+        <ul>
+          <li>tops</li>
+          <li>bottoms</li>
+        </ul>
+        <ul>
+          <li>skirts</li>
+          <li></li>
+        </ul>
+        <ul>
+          <li>hej</li>
+          <li>hej</li>
+        </ul>
+      </StyledAllLinks>
+    </StyledWrapper>
+  );
 }
+
+const StyledWrapper = styled.div`
+  background-color: #fff8ea;
+  color: red;
+  display: flex;
+  justify-content: space-between;
+  padding: 3px;
+`;
+
+const StyledLogo = styled.div`
+  background-color: #fff8ea;
+  color: red;
+`;
+
+const StyledAllLinks = styled.div`
+  display: flex;
+`;
